a006aaf1c741eaded2ee49cfb02b69fc6448661e
0xSorcerers <121747420+0xsorcerers@users.noreply.github.com>
2023-03-12 09:16:15 +0100

    Update Xenbox Governance Token - XBOX Minter.sol
    Enhanced Security practices

--- xenomorph/Xenbox Governance Token - XBOX Minter.sol
+++ xenomorph/Xenbox Governance Token - XBOX Minter.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-// By OxSorcerer | Battousai Nakamoto | DarcViper for Xenbox Games
+// @title Xenomorph NFT Game by OxSorcerer | Battousai Nakamoto | DarcViper for Xenbox Games
 // https://twitter.com/0xSorcerers | https://github.com/Dark-Viper | https://t.me/Oxsorcerer | https://t.me/battousainakamoto | https://t.me/darcViper
 
 pragma solidity ^0.8.17;
@@ -12,15 +12,23 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract Xenbox is ERC20, Ownable, ReentrancyGuard {        
-        constructor(string memory _name, string memory _symbol) 
+        constructor(string memory _name, string memory _symbol, address _newGuard) 
             ERC20(_name, _symbol)
-        {}
+        {
+            guard = _newGuard;
+        }
     using ABDKMath64x64 for uint256;
     using SafeMath for uint256;
 
     bool public paused = false;
+    address public guard;
     uint256 public CIRC_SUPPLY = 0;
     uint256 public MAX_SUPPLY = 5000000000 * 10 ** decimals();
+
+    modifier onlyGuard() {
+        require(msg.sender == guard, "Not authorized.");
+        _;
+    }
 
     event mintEvent(uint256 indexed multiplier);
     function mint(uint256 _multiplier) external onlyOwner {        
@@ -42,7 +50,7 @@ contract Xenbox is ERC20, Ownable, ReentrancyGuard {
     }
 
     event Pause();
-    function pause() public onlyOwner {
+    function pause() public onlyGuard {
         require(msg.sender == owner(), "Only Deployer.");
         require(!paused, "Contract already paused.");
         paused = true;
@@ -50,10 +58,14 @@ contract Xenbox is ERC20, Ownable, ReentrancyGuard {
     }
 
     event Unpause();
-    function unpause() public onlyOwner {
+    function unpause() public onlyGuard {
         require(msg.sender == owner(), "Only Deployer.");
         require(paused, "Contract not paused.");
         paused = false;
         emit Unpause();
+    }
+
+    function setGuard (address _newGuard) external onlyGuard {
+        guard = _newGuard;
     }
 }
