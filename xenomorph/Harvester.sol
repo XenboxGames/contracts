f3cbc0cf2ec91d973165f11c168eaddeb9d7c428
0xSorcerers <121747420+0xsorcerers@users.noreply.github.com>
2023-03-20 21:47:22 +0100

    Update and rename Incentivizer.sol to Harvester.sol

--- xenomorph/Harvester.sol
+++ xenomorph/Harvester.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 /**
  * @title Incentivizer Contract
  */
-contract Incentivizer is Ownable, ReentrancyGuard {
+contract Harvester is Ownable, ReentrancyGuard {
     IERC20 public xenboxToken;
     IERC20 public payToken;
     uint256 public totalRewards = 1;
